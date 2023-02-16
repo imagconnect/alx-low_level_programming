cdb27de4e256af8fb991d5d103b3ffe338f96598
imagconnect <nsisongpedet@gmail.com>
2023-02-16 15:24:25 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - print "Programming is like building a multilingual puzzle
+ * followed by newline'
+ *
+ * Return: Always 0 (Success)
+ */
+int main() {
+ 
+  char str1[] = "\"Programming is like building a multilingual puzzle";
+
+  puts(str1);
+
+  return 0;
+}
