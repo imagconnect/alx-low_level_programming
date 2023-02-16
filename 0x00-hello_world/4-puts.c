88d1224b1d3c3771a2f88b69e1d0eac8125ac38c
imagconnect <nsisongpedet@gmail.com>
2023-02-16 15:32:59 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,9 +9,8 @@
  */
 int main(void) {
  
-  char str1[] = "\"Programming is like building a multilingual puzzle";
 
-  puts(str1);
+  puts("\"Programming is like building a multilingual puzzle");
 
   return 0;
 }
