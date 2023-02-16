95de6dcdd4062c052b894d6cabf2044337b59dab
imagconnect <nsisongpedet@gmail.com>
2023-02-16 15:44:23 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print "Programming is like building a multilingual puzzle
+ * followed by newline'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
